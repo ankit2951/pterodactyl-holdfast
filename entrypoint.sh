eff58e1c396ec242ab2b9248bd92c33a6c94f767
BMGHosting <63407777+ankit2951@users.noreply.github.com>
2020-10-14 22:18:40 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 cd /home/container
 
-# Used for subscribing to betas or for testing
-if [ -z "$STEAMCMD_UPDATE_ARGS" ]; then
-	./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update "$STEAMAPPID" +quit
-else
-	steamcmd_update_args=($STEAMCMD_UPDATE_ARGS)
-	./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update "$STEAMAPPID" "${steamcmd_update_args[@]}" +quit
-fi
-
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
